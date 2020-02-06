c8b7d63cc6b3bc24c447ac6782a3417995befb27
odai-tech <odai195@gmail.com>
2020-02-06 12:13:32 +0200

    ok

--- app/src/main/java/com/example/user/odaiaoc2019/ProfileActivity.java
+++ app/src/main/java/com/example/user/odaiaoc2019/ProfileActivity.java
@@ -1,13 +1,41 @@
 package com.example.user.odaiaoc2019;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_CODE =1000 ;
+    Button mCaptureBtn;
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.capture_image_btn);
+
+        mCaptureBtn.setOnClickListener(new View.OnClickListener()  {
+        @Override
+        public void onClick(View v) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                 PackageManager.PERMISSION_DENIED | |
+                 checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                 PackageManager.PERMISSION_DENIED) {
+               String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                requestPermissions(permission, PERMISSION_CODE);
+                }
+                }
+
+            }
+        } 0     }
 }
+
+
+
